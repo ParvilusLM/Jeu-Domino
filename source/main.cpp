99806e10787c07166dfa48cb62efbf9f1705a961
Parvilus Love Marckendy <lovemarckendyparvilus@gmail.com>
2020-10-28 12:44:19 -0400

    1ere sauvegarde

---
+++ source/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+
+
+int main()
+{
+    srand(time(0));
+
+    sf::RenderWindow fenetre(sf::VideoMode(900,700),"Domino");
+    fenetre.setVerticalSyncEnabled(true);
+
+    while(fenetre.isOpen())
+    {
+        sf::Event evenement;
+        while(fenetre.pollEvent(evenement))
+        {
+            if(evenement.type==sf::Event::Closed)
+            {
+                fenetre.close();
+            }
+        }
+
+        fenetre.clear(sf::Color(82,82,82));
+
+        fenetre.display();
+    }
+
+    return 0;
+}
